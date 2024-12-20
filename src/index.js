1e01274657737c53a1ff4cb083480f7f2b93caef
MSalmaanTariq <123748279+MSalmaanTariq@users.noreply.github.com>
2024-12-20 19:14:20 +0500

    Added forward ref support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense, forwardRef, lazy } from "react";
 
-const loadable = (importFunc, { fallback = null } = { fallback: null }) => {
+const loadable = (
+  importFunc,
+  { fallback = null, withForwardRef = false } = { fallback: null }
+) => {
   const LazyComponent = lazy(importFunc);
 
+  if (withForwardRef) {
+    return forwardRef((innerProps, ref) => (
+      <Suspense fallback={fallback}>
+        <LazyComponent {...innerProps} ref={ref} />
+      </Suspense>
+    ));
+  }
+
+  // Regular case: return a function
   return (props) => (
     <Suspense fallback={fallback}>
       <LazyComponent {...props} />
